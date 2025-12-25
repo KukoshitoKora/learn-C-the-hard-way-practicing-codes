0f01c0d820ddb83ffe6795bd30964684ff4f9b22
Kukoshitokora <kumotobi@163.com>
2025-12-25 20:16:46 +0800

    update

---
+++ learn C the hard way/1.Dust Off That Compiler/ex1hw1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Hello world\nHello World\tHello world\n");
+	return 0;
+}
---
+++ learn C the hard way/1.Dust Off That Compiler/ex1hw2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+	puts("Hello world\nHello World\tHello world\n");
+	puts("Hello world\nHello World\tHello world\n");
+	puts("Hello world\nHello World\tHello world\n");
+	puts("Hello world\nHello World\tHello world\n");
+	puts("Hello world\nHello World\tHello world\n");
+	puts("Hello world\nHello World\tHello world\n");
+	return 0;
+}
